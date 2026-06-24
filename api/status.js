fbf91b06f071b14b4f65139f4ff642da3eb39702
Peanut44 <Peanutmakunat77@gmail.com>
2026-06-24 11:26:52 +0800

    Delete api directory

--- api/status.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Cache-Control", "no-store");
-  res.status(200).json({ status: "online", server: "mouseclick-vercel-v9", mode: "worker-playlist-host" });
-}
