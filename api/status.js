f03898b928888727a4dc834373a20c8d7cf68039
Peanut44 <Peanutmakunat77@gmail.com>
2026-06-24 12:31:24 +0800

    Delete api directory

--- api/status.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).json({ status: 'online', server: 'mouseclick-direct-vercel-test', mode: 'direct-bozztv-proxy' });
-}
